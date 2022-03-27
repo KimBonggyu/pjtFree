fc16fd7625c61d4ad59927224bf8e5421b702511
kbk <kbgkbg1999@gmail.com>
2022-03-27 18:32:21 +0900

    Error Code
    I got an error in entity setting code, but I haven't fixed it yet.
    
    * error :NoClassDefFoundError : org/apache/commons/beanutils/BeanUtils

---
+++ Coffee/src/main/java/com/coffee/util/EntityUtil.java
@@ -0,0 +1,40 @@
+package com.coffee.util;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class EntityUtil {
+
+   public static <T> T bindEntity(T entity) {
+	   return bindEntity(entity, null);
+   }
+   public static <T> T bindEntity(T entity, HttpServletRequest request) {
+	   
+	   try {   BeanUtils.setProperty(entity, "regNo", "admin");   } catch (Exception ignored) { /* 에러 무시 */ }
+	   try {   BeanUtils.setProperty(entity, "modNo", "admin");   } catch (Exception ignored) { /* 에러 무시 */ }
+	   
+	   if(request == null) {
+		   try {   BeanUtils.setProperty(entity, "regUrl", "reqNullRegUrl");   } catch (Exception ignored) { /* 에러 무시 */ }
+		   try {   BeanUtils.setProperty(entity, "modUrl", "reqNullModUrl");   } catch (Exception ignored) { /* 에러 무시 */ }
+	   }
+	   if(request != null) {
+		   try {   BeanUtils.setProperty(entity, "regUrl", request.getRequestURI());   } catch (Exception ignored) { /* 에러 무시 */ }
+		   try {   BeanUtils.setProperty(entity, "modUrl", request.getRequestURI());   } catch (Exception ignored) { /* 에러 무시 */ }
+	   }
+	   
+	   try {   BeanUtils.setProperty(entity, "regDts", new Date());   } catch (Exception ignored) { /* 에러 무시 */ }
+	   try {   BeanUtils.setProperty(entity, "modDts", new Date());   } catch (Exception ignored) { /* 에러 무시 */ }
+	   
+	   log.debug("newEntity: {}", entity);
+	   
+	   return entity; 
+   }
+   
+   
+}
